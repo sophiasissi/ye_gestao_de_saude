06a771751f6be4f493795565dafbff3a317c18e4
Léo Vieira <leopeixoto06@gmail.com>
2024-05-13 18:53:15 -0300

    finalização visual tela de pressão arterial

--- lib/telas/telas_historico_de_afericao_mensuracao/tela_pressao_arterial.dart
+++ lib/telas/telas_historico_de_afericao_mensuracao/tela_pressao_arterial.dart
@@ -6,7 +6,7 @@ class PressaoArterial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Pressão Arterial",
@@ -15,9 +15,98 @@ class PressaoArterial extends StatelessWidget {
         centerTitle: true,
         backgroundColor: CoresPrincipais.corTema,
       ),
-      body: const Column(
+      body: Column(
+        children: [
+          Column( // Ajuste conforme necessário
+            children: [
+              const Divider(height: 1,),
+              Row( // Substitua ListView por Row
+              children: <Widget>[
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  color: Colors.grey.shade200,
+                  alignment: Alignment.center,
+                  child: const Text("05/07/2020", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
+                ),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  color: CoresPrincipais.corTema,
+                  alignment: Alignment.center,
+                  child: const Text("120 X 80", style: TextStyle(color: Colors.white, fontSize: 18)),
+                ),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  color: Colors.grey.shade200,
+                  alignment: Alignment.center,
+                  child: const Text("Normal", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
+                ),
+              ],
+            ),
+            const Divider(height: 1,),
+              Row( // Substitua ListView por Row
+              children: <Widget>[
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  color: Colors.grey.shade200,
+                  alignment: Alignment.center,
+                  child: const Text("06/07/2020", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
+                ),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  color: CoresPrincipais.corTema,
+                  alignment: Alignment.center,
+                  child: const Text("110 X 70", style: TextStyle(color: Colors.white, fontSize: 18)),
+                ),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  color: Colors.grey.shade200,
+                  alignment: Alignment.center,
+                  child: const Text("Normal", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
+                ),
+              ],
+            ),
+            const Divider(height: 1,),
+              Row( // Substitua ListView por Row
+              children: <Widget>[
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  color: Colors.grey.shade200,
+                  alignment: Alignment.center,
+                  child: const Text("07/07/2020", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
+                ),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  color: CoresPrincipais.corTema,
+                  alignment: Alignment.center,
+                  child: const Text("150 X 100", style: TextStyle(color: Colors.white, fontSize: 18)),
+                ),
+                Container(
+                  height: 60,
+                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  color: Colors.grey.shade200,
+                  alignment: Alignment.center,
+                  child: const Text("Alta", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
+                ),
+              ],
+            ),
+            const Divider(height: 1,),
+          ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.grey.shade200,
+        child: const Icon(Icons.add, color: CoresPrincipais.corTema),
       ),
     );
   }
-  
 }
