0dbd74d24ec3729982d7d540a18bceb54ca479ce
Léo Vieira <leopeixoto06@gmail.com>
2024-05-13 19:32:21 -0300

    criação da tela de glicemia e retoques na tela de pressão arterial

--- lib/telas/telas_historico_de_afericao_mensuracao/tela_glicemia.dart
+++ lib/telas/telas_historico_de_afericao_mensuracao/tela_glicemia.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:ye_gestao_de_saude/_comum/cores.dart';
 
-class PressaoArterial extends StatelessWidget {
-  const PressaoArterial({super.key});
+class Glicemia extends StatelessWidget {
+  const Glicemia({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          "Pressão Arterial",
+          "Glicemia",
           style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 25),
         ),
         centerTitle: true,
@@ -17,28 +17,28 @@ class PressaoArterial extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Column( // Ajuste conforme necessário
+          Column( 
             children: [
               const Divider(height: 1,),
-              Row( // Substitua ListView por Row
+              Row( 
               children: <Widget>[
                 Container(
                   height: 60,
-                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  width: MediaQuery.of(context).size.width / 3, 
                   color: Colors.grey.shade200,
                   alignment: Alignment.center,
                   child: const Text("05/07/2020", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
                 ),
                 Container(
                   height: 60,
-                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  width: MediaQuery.of(context).size.width / 3, 
                   color: CoresPrincipais.corTema,
                   alignment: Alignment.center,
-                  child: const Text("120 X 80", style: TextStyle(color: Colors.white, fontSize: 18)),
+                  child: const Text("85", style: TextStyle(color: Colors.white, fontSize: 18)),
                 ),
                 Container(
                   height: 60,
-                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  width: MediaQuery.of(context).size.width / 3, 
                   color: Colors.grey.shade200,
                   alignment: Alignment.center,
                   child: const Text("Normal", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
@@ -46,54 +46,54 @@ class PressaoArterial extends StatelessWidget {
               ],
             ),
             const Divider(height: 1,),
-              Row( // Substitua ListView por Row
+              Row( 
               children: <Widget>[
                 Container(
                   height: 60,
-                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  width: MediaQuery.of(context).size.width / 3, 
                   color: Colors.grey.shade200,
                   alignment: Alignment.center,
                   child: const Text("06/07/2020", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
                 ),
                 Container(
                   height: 60,
-                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  width: MediaQuery.of(context).size.width / 3, 
                   color: CoresPrincipais.corTema,
                   alignment: Alignment.center,
-                  child: const Text("110 X 70", style: TextStyle(color: Colors.white, fontSize: 18)),
+                  child: const Text("150", style: TextStyle(color: Colors.white, fontSize: 18)),
                 ),
                 Container(
                   height: 60,
-                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  width: MediaQuery.of(context).size.width / 3, 
                   color: Colors.grey.shade200,
                   alignment: Alignment.center,
-                  child: const Text("Normal", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
+                  child: const Text("Alta", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
                 ),
               ],
             ),
             const Divider(height: 1,),
-              Row( // Substitua ListView por Row
+              Row( 
               children: <Widget>[
                 Container(
                   height: 60,
-                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  width: MediaQuery.of(context).size.width / 3, 
                   color: Colors.grey.shade200,
                   alignment: Alignment.center,
                   child: const Text("07/07/2020", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
                 ),
                 Container(
                   height: 60,
-                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  width: MediaQuery.of(context).size.width / 3, 
                   color: CoresPrincipais.corTema,
                   alignment: Alignment.center,
-                  child: const Text("150 X 100", style: TextStyle(color: Colors.white, fontSize: 18)),
+                  child: const Text("100", style: TextStyle(color: Colors.white, fontSize: 18)),
                 ),
                 Container(
                   height: 60,
-                  width: MediaQuery.of(context).size.width / 3, // Ocupa 1/3 da tela
+                  width: MediaQuery.of(context).size.width / 3, 
                   color: Colors.grey.shade200,
                   alignment: Alignment.center,
-                  child: const Text("Alta", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
+                  child: const Text("Normal", style: TextStyle(color: CoresPrincipais.corTema, fontSize: 18)),
                 ),
               ],
             ),
